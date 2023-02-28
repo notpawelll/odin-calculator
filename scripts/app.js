4a1ada49fd58d2e1dc2699d4736b72538672d25c
Pawel Michalski <michalskipawel1998@gmail.com>
2023-02-28 15:36:09 -0500

    began implementation of state machine; some bugs w/ transitions still exist

--- scripts/app.js
+++ scripts/app.js
@@ -33,33 +33,183 @@ let accumulator = 0;
 let operandOne = 0;
 let operandTwo = 0;
 let operator = undefined;
+let currentState = "allClear";
 
 let calculatorDisplay = document.querySelector("#display");
 let digitButtons = document.querySelectorAll(".button-digit");
 let operatorButtons = document.querySelectorAll(".button-operator");
 let utilityButtons = document.querySelectorAll(".button-utility");
+let clearButton = document.querySelector("#utility-clear");
+
+function stateTransition(currentState, transitionType) {
+  let nextState;
+  switch (currentState) {
+    case "allClear":
+      switch (transitionType) {
+        case "clear":
+        case "equals":
+          nextState = "allClear";
+          break;
+        case "digit":
+          nextState = "operandOne";
+          break;
+        case "operator":
+          nextState = "operator";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    case "clear":
+      switch (transitionType) {
+        case "clear":
+        case "operator":
+          nextState = "allClear";
+          break;
+        case "digit":
+          nextState = "operandTwo";
+          break;
+        case "equals":
+          nextState = "result";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    case "operandOne":
+      switch (transitionType) {
+        case "clear":
+          nextState = "allClear";
+          break;
+        case "digit":
+        case "equals":
+          nextState = "operandOne";
+          break;
+        case "operator":
+          nextState = "operator";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    case "operator":
+      switch (transitionType) {
+        case "clear":
+          nextState = "operator";
+          break;
+        case "digit":
+          nextState = "operandTwo";
+          break;
+        case "equals":
+          nextState = "result";
+          break;
+        case "operator":
+          nextState = "operator";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    case "operandTwo":
+      switch (transitionType) {
+        case "clear":
+          nextState = "clear";
+          break;
+        case "operator":
+          nextState = "operator";
+          break;
+        case "digit":
+          nextState = "operandTwo";
+          break;
+        case "equals":
+          nextState = "result";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    case "result":
+      switch (transitionType) {
+        case "clear":
+          nextState = "clear";
+          break;
+        case "digit":
+          nextState = "operandOne";
+          break;
+        case "equals":
+          nextState = "result";
+          break;
+        case "operator":
+          nextState = "operator";
+          break;
+        default:
+          console.log(`attempted unsupported transition from state ${currentState} with transition ${transitionType}!`);
+          nextState = currentState;
+          break;
+      }
+      break;
+    default:
+      console.log(`currently in unknown state ${currentState}. resetting to allClear!`);
+      nextState = "allClear";
+      break;
+  }
+  console.log(`${currentState} ---(${transitionType})--> ${nextState}`);
+  return nextState;
+}
 
 function updateDisplay(newValue) {
   calculatorDisplay.textContent = newValue;
 }
 
-function updateOperator() {
+function updateClearButtonText(currentState) {
+  switch (currentState) {
+    case "allClear":
+    case "clear":
+      clearButton.textContent = "AC";
+      break;
+    default:
+      clearButton.textContent = "C";
+      break;
+  }
+}
+
+function highlightOperator(operatorId) {
   operatorButtons.forEach((operatorButton) => {
     operatorButton.classList.remove("button-operator-active");
-    if (operatorButton.id === operator) {
+    if (operatorButton.id === operatorId) {
       operatorButton.classList.add("button-operator-active");
     }
   });
 }
 
-function compute() {
-  accumulator = operate(operandOne, operandTwo, operator);
+function appendToOperand(operandValue, digitValue) {
+  return operandValue === 0 ? digitValue : Number(`${operandValue}${digitValue}`);
 }
 
 function pressDigitButton(digitValue) {
-  console.log(digitValue);
-  accumulator = accumulator === 0 ? digitValue : Number(`${accumulator}${digitValue}`);
-  updateDisplay(accumulator);
+  currentState = stateTransition(currentState, "digit");
+  switch (currentState) {
+    case "operandOne":
+      operandOne = appendToOperand(operandOne, digitValue)
+      updateDisplay(operandOne);
+      break;
+    case "operandTwo":
+      operandTwo = appendToOperand(operandTwo, digitValue);
+      updateDisplay(operandTwo);
+      break;
+    default:
+      break;
+  }
 }
 
 function pressOperatorButton(operatorId) {
@@ -68,41 +218,77 @@ function pressOperatorButton(operatorId) {
     case "operator-times":
     case "operator-minus":
     case "operator-plus":
+      currentState = stateTransition(currentState, "operator");
       operator = operatorId;
-      updateOperator();
-    case "operator-equals":
-      compute();
-      operandOne = accumulator;
       operandTwo = 0;
-      operator = undefined;
-      updateDisplay(operandOne);
-      updateOperator();
+      highlightOperator(operatorId);
+      break;
+    case "operator-equals":
+      currentState = stateTransition(currentState, "equals");
+      accumulator = operate(operandOne, operandTwo, operator);
+      console.log(`${operandOne} ${operator} ${operandTwo} = ${accumulator}`);
+      highlightOperator(null);
+      updateDisplay(accumulator);
+      operandOne = accumulator;
       break;
     default:
-      operator = undefined;
       break;
   }
-  console.log(operatorId, operator);
 }
 
-function pressUtilityButton(buttonId) {
-  console.log(buttonId);
+function pressUtilityButton(utilityId) {
+  switch (utilityId) {
+    case "utility-clear":
+      currentState = stateTransition(currentState, "clear");
+      switch (currentState) {
+        case "clear":
+          operandOne = operandTwo;
+          operandTwo = 0;
+          accumulator = 0;
+          highlightOperator(operator);
+          updateDisplay(operandTwo);
+          break;
+        case "allClear":
+          operandOne = 0;
+          operandTwo = 0;
+          operator = undefined;
+          accumulator = 0;
+          highlightOperator(null);
+          updateDisplay(operandOne);
+          break;
+        case "operandOne":
+          operandOne = 0;
+          updateDisplay(operandOne);
+          break;
+        case "operator":
+          operandTwo = 0;
+          updateDisplay(operandTwo);
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 digitButtons.forEach((digitButton) => {
   digitButton.addEventListener("click", e => {
     pressDigitButton(Number(digitButton.textContent));
+    updateClearButtonText(currentState);
   });
 });
 
 operatorButtons.forEach((operatorButton) => {
   operatorButton.addEventListener("click", e => {
     pressOperatorButton(e.target.id);
+    updateClearButtonText(currentState);
   });
 });
 
 utilityButtons.forEach((utilityButton) => {
   utilityButton.addEventListener("click", e => {
     pressUtilityButton(e.target.id);
+    updateClearButtonText(currentState);
   });
 });
